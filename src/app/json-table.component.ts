4614aa8acfb5a11a529b603b933e8c0566632fc5
Matt Payne <Payne@MattPayne.org>
2025-01-17 20:57:01 -0600

    wip

---
+++ src/app/json-table.component.ts
@@ -0,0 +1,60 @@
+// json-table.component.ts
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatTableModule, MatTable } from '@angular/material/table';
+import { MatSortModule, MatSort } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'app-json-table',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  templateUrl: './json-table.component.html',
+  styleUrls: ['./json-table.component.scss']
+})
+export class JsonTableComponent implements OnInit {
+  @Input() dataUrl!: string;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<any>;
+
+  dataSource: MatTableDataSource<any> = new MatTableDataSource();
+  displayedColumns: string[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.http.get(this.dataUrl).subscribe({
+      next: (data: any) => {
+        if (Array.isArray(data) && data.length > 0) {
+          // Get column names from the first object
+          this.displayedColumns = Object.keys(data[0]);
+          this.dataSource = new MatTableDataSource(data);
+          this.dataSource.sort = this.sort;
+        }
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+      }
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
+
